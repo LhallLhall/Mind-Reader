040eb5a44552f7ec60a277d56a1bc123e786b2e7
Logan Hall <logan.hall@student.awesomeinc.org>
2022-09-21 15:35:34 +0000

    made reset button non visible whe it;s on page 0

--- js/main.js
+++ js/main.js
@@ -1,1 +1,101 @@
 
+ // this is the init phase
+ // create variables and access html elements in global scope
+let nextBtn = document.getElementById("nextBtn");
+let goBtnText = document.getElementById("nextBtn")
+// let prevBtn = document.getElementById("prevBtn");
+let resetBtn = document.getElementById("resetBtn");
+// let resetBtnText = document.getElementById("resetBtn");
+let instructionText = document.getElementById("instructionText");
+let exampleText = document.getElementById("exampleText");
+
+// this is the state
+// the state keeps the "current page" as an integer
+// the mainPages array holds all of the page objects which can be called back to
+let pageNumber = 0;
+
+let mainPages = [
+    { // page 1
+        "instructionText": "first page Header", 
+        // "exampleText": "this is the first page Example text"
+        "nextBtn": "GO",
+    },
+    { //page 2
+        "instructionText": "second page Header",
+        "nextBtn": "Next",
+        "exampleText": "this is the second page Example text"
+    },
+    { //page 3
+        "instructionText": "third page Header",
+        "nextBtn": "Next",
+        "exampleText": "this is the third page Example text"
+    },
+    { //page 4
+        "instructionText": "fourth page Header",
+        "nextBtn": "Next", 
+        "exampleText": "this is the fourth page Example text"
+    },
+    { //page 5
+        "instructionText": "fifth page Header", //will need the randomized symbols pushed into it (MAYBE (along with a stirng of numbers))
+        "nextBtn": "Reveal", 
+        "exampleText": "this is the fifth page Example text"
+    },
+    { //page 6
+        "instructionText": "sixth page Header",
+        "nextBtn": "blah", 
+        "exampleText": "this is the sixth page Example text"
+    }
+];
+
+// create the event listeners for the "on click" buttons
+// call the updatePage method when any button is clicked
+nextBtn.addEventListener("click", updatePage);
+resetBtn.addEventListener("click", updatePage);
+// prevBtn.addEventListener("click", updatePage);
+
+// this is the return for the listener
+function updatePage (e) {
+    // console.log(e.target.id)
+    // assume we are on page 0
+    // mainPages[0]
+    
+    // adjust the pageNumber we are on by +1, -1, or reset to 0
+    // if e.target.id == "nextBtn"
+    if(e.target.id == "nextBtn"){
+        pageNumber++;
+        
+    }
+    
+    // if e.target.id == "prevBtn"
+    // if(e.target.id == "prevBtn"){
+        //     pageNumber--;
+        // }
+        
+        // if e.target.id == "resetBtn"
+        if(e.target.id == "resetBtn"){
+            pageNumber = 0;
+            // document.getElementById("resetBtn").style.display = "none";
+        }
+        
+        // use the property values from the current page based on the mainPages index set by the pageNumber
+        instructionText.textContent = mainPages[pageNumber].instructionText;
+        exampleText.textContent = mainPages[pageNumber].exampleText;
+        nextBtn.textContent = mainPages[pageNumber].nextBtn;
+        
+    };
+    // figure out how to hide elements with a function
+    // defintely could be a simplier way to do this with the object
+    function hideElements () {
+        // console.log(pageNumber)
+        if(pageNumber === 0 ) {
+            console.log(pageNumber)
+            // let resetBtnTest = document.createElement("button")
+            // resetBtnTest.setAttribute("id", "resetBtn1");
+            document.getElementById("resetBtn").style.visibility = "none"
+        }// if (pageNumber > 0) {
+        //     document.getElementById("resetBtn").classList.remove("hidden")
+        // }
+    }
+    hideElements()
+    
+    
--- pseudocode.md
+++ pseudocode.md
@@ -1,23 +1,23 @@
 ## questions for Logan
-- how to call upon an array or even what to do with it <!-- maybe look back at previous projects>
-- need to figure out how many variables i will need
-- need to understand what state is more clearly
 - 
 
 ## Variables
 - resetBtn
 - nextBtn
 - goBtn
+- goBtnText <!-- will probaby end up taking this off when trimming-->
 - symbols
+- instructionText <!-- the text that is underneath the next btn. Grabs the id of the h1-->
+- exampleText <!-- grabs the p element-->
 
 ## state
 - symbols: [](9)
 - randomized sum: [](100)
 - answer sum : integer
-- current page:
+- current page: integer
 
 ## objects
-- mainPages []
+1 mainPages []
   - instructionText: " " <!-- "pick a number", "add both digits together to get a new number", "subtract your new number with the original number" would also maybe turn into the main display for all the symbols-->
   - ExampleText: " " <!-- I.E the "ex. 14 is 1 - 4 = 5" -->
   - pageNum: 0 <!-- page num would be updated//+1 on the "next" btn click. if the reset button on click it would -1-->
@@ -27,13 +27,36 @@
 
 ## functions
 - updatePage()
-  - 
+  ~~~
+  if target id == id of btn
+    add +1 to the PageNumber
+
+  If target id == id of reset btn
+    subtract 1 to the pageNumber
+  ~~~
 - RandomizeSymbols()
-- reset()
-- nextPage()
+  ~~~
+
+  ~~~
+- reset() <!-- tbd -->
+- nextPage()<!-- tbd -->
 - init()
 
-<!-- first thing the js would check is the what page it was on (figure out how to do that using the mainPages obj) -->
-<!-- second thing would be to display all items for that page -->
-<!-- thirdly once "next" or "go" if on page 1 is clicked it resets page and checks what the next page is and applies said elements (could be a logic error with the reset and the +1 page num on click) -->
-<!-- going deeper into the third. things that would be reset are [example text (would not be on pages )] -->
+- First: the first thing the js would check is the what page it was on (figure out how to do that using the mainPages obj) 
+- Second: next thing would be to display all items for that page 
+- Third: once "next" if on page "2" is clicked it resets page and checks what the next page is and applies said elements (could be a logic error with the reset and the +1 page num on click) 
+  - Going deeper into the third. things that would be reset are [instructionText, exampleText, and the symbols for page 5.(would not be on pages)]
+   - Going even deeper 
+- Fourth: when 
+
+
+when on page "1" run a function that 
+if pagenumber === 0 
+  make style.display = none <!-- to make the btn ivisible-->
+  else pagenumber > 0
+   display the btn
+
+
+<!-- If page == 1 
+  make a variable that hides html elements when called
+  or get element by id  -->
